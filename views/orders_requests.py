e6c58d0764d36ad5937e5cb8aaa91e29d7d83755
Eric Lehning <ellehning@gmail.com>
2023-04-12 12:00:49 -0500

    startup

---
+++ views/orders_requests.py
@@ -0,0 +1,17 @@
+ORDERS = [
+    {
+        "id": 1,
+        "product_id": 1,
+        "employee_id": 1,
+        "timestamp": "20230412" 
+    },
+    {
+        "id": 2,
+        "product_id": 2,
+        "employee_id": 2,
+        "timestamp": "20230412"
+    }
+]
+
+def get_all_orders():
+    return ORDERS
---
+++ views/products_requests.py
@@ -0,0 +1,20 @@
+PRODUCTS = [
+    {
+        "id": 1,
+        "name": "black coffee",
+        "price": 5
+    },
+    {
+        "id": 2,
+        "name": "espresso",
+        "price": 6
+    },
+    {
+        "id": 3,
+        "name": "cookie",
+        "price": 3
+    }
+]
+
+def get_all_products():
+    return PRODUCTS
