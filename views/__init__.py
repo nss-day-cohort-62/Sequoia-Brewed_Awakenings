9a04cce801c2556f7305fb6cc28f8e9aa48f256a
Eric Frey <ericlfrey@gmail.com>
2023-04-13 09:57:34 -0500

    Finished collab session number 2. Agged get_single_resources functionality and tested all requests in Postman.

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
-from .employees_requests import get_all_employees
-from .orders_requests import get_all_orders
-from .products_requests import get_all_products
+from .employees_requests import get_all_employees, get_single_employee
+from .orders_requests import get_all_orders, get_single_order
+from .products_requests import get_all_products, get_single_product
